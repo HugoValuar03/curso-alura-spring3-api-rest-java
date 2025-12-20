ab0b216726330e3a8800b5ea1f23b8d903756d18
Hugo Valuar <hugovaluarb03@gmail.com>
2025-12-20 01:18:30 -0300

    Adiciona ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,16 +28,25 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) { //@PagealeDefault cria padrões caso não seja informado na url
         return repository.findAll(paginacao).map(DadosListagemMedico::new); //Converte uma lista de médicos para uma lista de DadosListagemMedico
     }
 
     /*
-    * Para controlar quantos registros vem por página, na url após o localhost:8080/medicos, se passa os seguintes parametros:
-    * ?size=1 -> Para controlar quantas páginas irão vir
-    * Após o ?size=1 pode se passar o page que apresenta a página atual que inicia no 0.
-    * Se quiser mostrar a 10 registros e apresentar a segunda página a URL é esta:
-    * "localhost:8080/medicos?size=10&page=1"
-    */
+     * Para controlar quantos registros vem por página, na url após o localhost:8080/medicos, se passa os seguintes parametros:
+     * ?size=1 -> Para controlar quantas páginas irão vir
+     * Após o ?size=1 pode se passar o page que apresenta a página atual que inicia no 0.
+     * Se quiser mostrar a 10 registros e apresentar a segunda página a URL é esta:
+     * "localhost:8080/medicos?size=10&page=1"
+     */
+
+    /*
+     * Para realziar a ordenação, também é pela url
+     * Se quiser ordernar por nome do médico, basta passar o nome atributo na entidade jpa, a url seria:
+     * -> "localhost:8080/medicos?sort=nome"
+     * Se quiser ordernar de forma decrescente:
+     * -> "localhost:8080/medicos?sort=nome,desc"
+     *
+     */
 
 }
